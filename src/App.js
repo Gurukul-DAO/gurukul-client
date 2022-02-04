cfe3007668372ee5fbb070554ad162c3d3581e95
Flaviu Cicio <flaviucicio@gmail.com>
2022-02-04 10:30:10 +0200

    Setup basic react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Gurukul</h1>
+        <p>Under development</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
