7cf6238e46275a4ae4db373a76c43925cd47bd0b
ChrisiPK <ChrisiPK@gmail.com>
2022-03-06 19:17:12 +0100

    change moralis app ID and server URL

--- src/credentials.js
+++ src/credentials.js
@@ -1,5 +1,5 @@
-export const moralisAppId="vyLBWoHA66r9ezXEiKVbgEEGnxgqUYH0IzvMeC1b";
-export const moralisServerUrl="https://kuktxedpoti5.usemoralis.com:2053/server";
+export const moralisAppId="tNkjFWQOe4KE82kqPbm6wR5jmKMlOQpG9xuLf6Rv";
+export const moralisServerUrl="https://crltoemzxtfr.usemoralis.com:2053/server";
 export const gurukulContractAddress="0x9FDc9E3CB50Da42B46c9841f1F97dc21B084A7E8";
 export const guruContractAddress="0x76ae495345A42b78f6A2e730556A498DfA3B6816";
 export const nftPortApiKey="3f5abead-c993-4632-b42a-ea6b72a733a8";
